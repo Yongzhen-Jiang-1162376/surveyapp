1a3384597aebd9e3265b02c10081e91ea7105bd3
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-08-05 13:57:31 +1200

    add image variations list controller

---
+++ project693/controller/image_controller.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, session
+from project693.controller import app
+from werkzeug.utils import secure_filename
+from project693.dao.plant_dao import PlantDAO
+from project693.utils.session_manager import SessionManager
+import os, uuid, json
+from project693.data.dall_e_2_image_variations import dall_e_2_images_variations
+
+
+@app.route("/misc/image_variations", methods=["GET"])
+def list_image_variations():
+    SessionManager.set(
+        SessionManager.ACTIVE_PAGE, SessionManager.Page.LIST_IMAGE_VARIATIONS.value
+    )
+
+    return render_template(
+        "misc/image_variations.html", images=dall_e_2_images_variations
+    )
