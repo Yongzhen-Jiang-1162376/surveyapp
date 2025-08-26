6be111de3c53470393bfb5a2d20b8a7ee872b505
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-08-26 15:42:07 +1200

    fix survey data fetch using post rather than get, and add api for download

--- project693/controller/api_controller.py
+++ project693/controller/api_controller.py
@@ -12,10 +12,13 @@ from datetime import datetime
 analysis_dao = AnalysisDAO()
 
 
-@app.route("/api/current-survey-data", methods=["GET"])
+@app.route("/api/current-survey-data", methods=["POST"])
 def get_survey_data():
-    page = int(request.args.get("page", 1))
-    limit = int(request.args.get("limit", 10))
+    # page = int(request.args.get("page", 1))
+    # limit = int(request.args.get("limit", 10))
+    req = request.get_json()
+    page = int(req.get("page", 1))
+    limit = int(req.get("limit", 10))
     
     offset = (page - 1) * limit
     
@@ -43,4 +46,32 @@ def get_survey_data():
     return jsonify({
         "datatable": datatable,
         "total": total
+    })
+
+
+@app.route("/api/all-current-survey-data", methods=["POST"])
+def get_all_survey_data():
+    
+    rows = analysis_dao.list_all_current_survey_results_with_plant_name()
+    
+    columns = [
+        'session_id',
+        'question_seq',
+        'submission_time',
+        'response_time',
+        'invasive_plant_id',
+        'invasive_plant_name',
+        'non_invasive_plant_id',
+        'non_invasive_plant_name',
+        'selected_plant_id',
+        'selected_plant_name',
+        'has_garden',
+        'age_group',
+        'reasoning',
+        'invasive_win'
+    ]
+    datatable = [dict(zip(columns, row)) for row in rows]
+    
+    return jsonify({
+        "datatable": datatable
     })
