a0808f2158ac44675ae7fc210a01810f61f078ec
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-09-08 23:06:29 +1200

    add survey cycle support

---
+++ project693/controller/survey_cycle_controller.py
@@ -0,0 +1,17 @@
+from flask import request, render_template, redirect, url_for, session, flash
+from project693.controller import app
+from project693.utils.session_manager import SessionManager
+from project693.dao.plant_dao import PlantDAO
+from project693.dao.survey_dao import SurveyDAO
+from project693.model.survey import SurveyMetadata, SurveyAnswer
+import uuid
+from datetime import datetime
+
+
+plant_dao = PlantDAO()
+survey_dao = SurveyDAO()
+
+
+@app.route("/siteadmin/survey_cycle", methods=["GET"])
+def survey_cycle():
+    return render_template("admin/survey_cycle.html")
