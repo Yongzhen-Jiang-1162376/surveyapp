e0db2c05559d897bca3b4c9351b07dc14dddf53f
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2025-09-11 00:39:42 +1200

    fix home page in case no survey cycle available

--- project693/controller/home_controller.py
+++ project693/controller/home_controller.py
@@ -4,6 +4,7 @@ import platform
 from flask import session
 from project693.controller import app
 from project693.utils.session_manager import SessionManager
+from project693.dao.survey_dao import SurveyDAO
 
 
 def format_date(date_obj):
@@ -13,4 +14,13 @@ def format_date(date_obj):
 @app.route("/home/", methods=["GET"])
 def site_home():
     SessionManager.set(SessionManager.ACTIVE_PAGE, SessionManager.Page.SITEHOME.value)
+    
+    survey_dao = SurveyDAO()
+    result = survey_dao.active_survey_cycle_existed()
+    
+    # if there is not current active survey cycle available, then start a new survey
+    # fallback for a new start of our application when the database is empty
+    if result[0][0] == 0:
+        survey_dao.start_survey_cyle()
+    
     return render_template("site_home.html")
