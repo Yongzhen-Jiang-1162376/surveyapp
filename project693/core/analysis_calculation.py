fixed weighted calculation

project693/core/analysis_calculation.py
@@ -78,15 +78,23 @@ def initialize():
     config['weighted_beta_normalized'] = calc_normalized_betas(config['weighted_betas'])
     config['unweighted_beta_normalized'] = calc_normalized_betas(config['unweighted_betas'])
 
+    print(config['data']['RT'])
 
 def weighted_log_likelihood(betas, data):
     ll = 0.0
+    
+    RT_max = config['data']['RT'].max()
+    RT_min = config['data']['RT'].min()
+    
     for _, row in data.iterrows():
         # mapping image id to index
         i= config['id_to_idx'][row['winner']]
         j = config['id_to_idx'][row['loser']]
         beta_i, beta_j = betas[i], betas[j]
-        weight = row['RT']       # weight
+        # weight = row['RT']       # weight
+        
+        weight = (RT_max - row['RT']) / (RT_max - RT_max + 1e-9)
+        
         p = np.exp(beta_i) / (np.exp(beta_i) + np.exp(beta_j))
         ll += weight * np.log(p + 1e-9)
     return -ll